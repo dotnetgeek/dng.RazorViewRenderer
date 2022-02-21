3cc913490bee23a362aa1da98895fd9283390bf1
Daniel Mueller <daniel@dotnetgeek.de>
2022-02-21 23:20:38 +0100

    Code rework

--- src/dng.RazorViewRenderer/RazorViewRenderer.cs
+++ src/dng.RazorViewRenderer/RazorViewRenderer.cs
@@ -1,15 +1,14 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.Options;
-
-using static System.String;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace dng.RazorViewRenderer;
 
@@ -17,16 +16,16 @@ public class RazorViewRenderer : IRazorViewRenderer
 {
     private readonly IRazorViewEngine _razorViewEngine;
     private readonly ITempDataProvider _tempDataProvider;
-    private readonly IServiceProvider _serviceProvider;
+    private readonly IHttpContextAccessor _httpContextAccessor;
 
     public RazorViewRenderer(
+       IHttpContextAccessor httpContextAccessor,
         IRazorViewEngine razorViewEngine,
-        ITempDataProvider tempDataProvider,
-        IServiceProvider serviceProvider)
+        ITempDataProvider tempDataProvider)
     {
         _razorViewEngine = razorViewEngine;
         _tempDataProvider = tempDataProvider;
-        _serviceProvider = serviceProvider;
+        _httpContextAccessor = httpContextAccessor;
     }
 
     public async Task<string> RenderViewToStringAsync<TModel>(
@@ -35,14 +34,7 @@ public class RazorViewRenderer : IRazorViewRenderer
     {
         var actionContext = GetActionContext();
 
-        var viewEngineResult = _razorViewEngine.FindView(actionContext, viewName, false);
-
-        if (!viewEngineResult.Success)
-        {
-            throw new InvalidOperationException(Format("Couldn't find view '{0}'", viewName));
-        }
-
-        var view = viewEngineResult.View;
+        var view = FindView(actionContext, viewName, _razorViewEngine);
 
         using (var output = new StringWriter())
         {
@@ -64,14 +56,38 @@ public class RazorViewRenderer : IRazorViewRenderer
         }
     }
 
+    private IView FindView(
+        ActionContext actionContext,
+        string viewName,
+        IRazorViewEngine viewEngine)
+    {
+        var getViewResult = viewEngine.GetView(executingFilePath: null, viewPath: viewName, isMainPage: false);
+        if (getViewResult.Success)
+            return getViewResult.View;
+
+        var findViewResult = viewEngine.FindView(actionContext, viewName, isMainPage: false);
+        if (findViewResult.Success)
+            return findViewResult.View;
+
+        var searchedLocations = getViewResult.SearchedLocations.Concat(findViewResult.SearchedLocations);
+        var errorMessage = string.Join(
+            Environment.NewLine,
+            new[] { $"Unable to find view '{viewName}'. The following locations were searched:" }.Concat(searchedLocations)); ;
+
+        throw new InvalidOperationException(errorMessage);
+    }
+
     private ActionContext GetActionContext()
     {
-        return new ActionContext(
-            new DefaultHttpContext()
-            {
-                RequestServices = _serviceProvider
-            },
-            new RouteData(),
-            new ActionDescriptor());
+        var httpContext = _httpContextAccessor.HttpContext;
+        var actionContext = httpContext?.RequestServices.GetService<IActionContextAccessor>()?.ActionContext;
+
+        if (actionContext != null)
+            return actionContext;
+
+        var routeData = new RouteData();
+        routeData.Routers.Add(new RouteCollection());
+
+        return new ActionContext(httpContext, routeData, new ActionDescriptor());
     }
 }
